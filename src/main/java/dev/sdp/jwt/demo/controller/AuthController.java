9a9d0eaa22f459a3a560b8882c4c08037f71fb3c
francis kinyuru <franciskinyuru26@gmail.com>
2023-05-24 13:03:57 +0300

    branch for generating a token via username and password and signing with private public key

--- src/main/java/dev/sdp/jwt/demo/controller/AuthController.java
+++ src/main/java/dev/sdp/jwt/demo/controller/AuthController.java
@@ -1,10 +1,14 @@
 package dev.sdp.jwt.demo.controller;
 
+import dev.sdp.jwt.demo.model.LoginRequest;
 import dev.sdp.jwt.demo.service.TokenService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,12 +16,15 @@ public class AuthController {
     private static final Logger LOG = LoggerFactory.getLogger(AuthController.class);
 
     private final TokenService tokenService;
+    private final AuthenticationManager authenticationManager;
 
-    public AuthController(TokenService tokenService) {
+    public AuthController(TokenService tokenService, AuthenticationManager authenticationManager) {
         this.tokenService = tokenService;
+        this.authenticationManager = authenticationManager;
     }
     @PostMapping("/token")
-    public String token(Authentication authentication){
+    public String token(@RequestBody LoginRequest loginRequest) {
+        Authentication authentication=authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginRequest.username(),loginRequest.password()));
         LOG.debug("token requested for user: '{}'",authentication.getName());
         String token=tokenService.generateToken(authentication);
         LOG.debug("Token granted: '{}'",token);
---
+++ src/main/java/dev/sdp/jwt/demo/model/LoginRequest.java
@@ -0,0 +1,5 @@
+package dev.sdp.jwt.demo.model;
+
+public record LoginRequest(String username, String password) {
+
+}

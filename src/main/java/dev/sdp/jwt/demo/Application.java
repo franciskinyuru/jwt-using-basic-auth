026c46517c1e5be148261467f6b87cded87e96be
francis kinyuru <franciskinyuru26@gmail.com>
2023-05-26 21:15:02 +0300

    small change

--- src/main/java/dev/sdp/jwt/demo/Application.java
+++ src/main/java/dev/sdp/jwt/demo/Application.java
@@ -1,5 +1,6 @@
 package dev.sdp.jwt.demo;
 
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties;
 import dev.sdp.jwt.demo.configs.RsaKeyProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 
 @EnableConfigurationProperties(RsaKeyProperties.class)
 @SpringBootApplication
+@EnableEncryptableProperties
 public class Application {
 
 	public static void main(String[] args) {
